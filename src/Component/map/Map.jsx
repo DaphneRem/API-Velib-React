a05a284814d4d2dde8b871ab112f1b49bc126167
daphne remali <remalidaphne@gmail.com>
2017-02-22 17:36:35 +0100

    add react-gmaps

---
+++ src/Component/map/Map.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Map.css';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+
+class Map extends React.Component {
+
+    render() {
+
+
+        return (
+            <div className="map">
+                <h1>MAP</h1>
+            </div>
+
+        );
+    }
+}
+export default Map;
